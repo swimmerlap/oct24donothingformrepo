0a07420cbacc162d0c03b7bdd145cf54b51e589b
swimmerlap <dizzgil@aol.com>
2018-10-24 15:20:20 -0400

    second

--- src/main/java/com/example/demo/HomeContoller.java
+++ src/main/java/com/example/demo/HomeContoller.java
@@ -6,7 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeContoller {
     @RequestMapping("/")
-    public String home(){
-        return "index";
+    public String login() {
+        return "login";
+    }
+
+
+    @RequestMapping("/welcome")
+    public String welcome (){
+        return "welcome";
     }
 }
+
+
+
